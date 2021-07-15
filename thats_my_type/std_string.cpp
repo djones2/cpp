5fa663014e2172f3afc91690c24dbbf4a1ad8a01
Daniel <dcjones2015@gmail.com>
2021-07-15 15:41:19 -0700

    String input

---
+++ thats_my_type/std_string.cpp
@@ -0,0 +1,18 @@
+#include <string> // For std::string and std::getline
+#include <iostream>
+#include <iomanip> // For std::ws
+
+int main()
+{
+    std::cout << "Enter your full name: ";
+    std::string name{};
+    std::getline(std::cin >> std::ws, name); // read a full line of text into name
+
+    std::cout << "Enter your age: ";
+    std::string age{};
+    std::getline(std::cin >> std::ws, age); // read a full line of text into age
+
+    std::cout << "Your name is " << name << " and your age is " << age << '\n';
+
+    return 0;
+}
